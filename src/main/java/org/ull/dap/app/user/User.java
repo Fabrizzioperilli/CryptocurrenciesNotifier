c1e25816482f52bbf48b60b002fea3450b8ff956
Fabrizzioperilli <alu0101138589@ull.edu.es>
2023-11-30 00:22:22 +0000

    Se añade la clase User, clase observador concreto.

---
+++ src/main/java/org/ull/dap/app/user/User.java
@@ -0,0 +1,44 @@
+package org.ull.dap.app.user;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+public class User implements IObserver {
+
+    private String name;
+    private long id;
+    private List<String> nameCryptos;
+    private Map<String, Double> cryptoPrices;
+
+    public User() {}
+
+    public User(String name, long id, List<String> nameCryptos) {
+        this.name = name;
+        this.id = id;
+        this.nameCryptos = nameCryptos;
+        this.cryptoPrices = new HashMap<>();
+
+        for (String nameCrypto : nameCryptos) {
+            cryptoPrices.put(nameCrypto, -1.0);
+        }
+    }
+
+    @Override
+    public void update(String nameCrypto, double newPrice) {
+        cryptoPrices.put(nameCrypto, newPrice);
+        System.out.println(name + " has been notified about " + nameCrypto +
+                ", the price has changed to: " + cryptoPrices.get(nameCrypto) + " USD");
+    }
+
+    @Override
+    public List<String> getNameCryptos() {
+        return nameCryptos;
+    }
+
+
+    public Map<String, Double> getCryptoPrices() {
+        return cryptoPrices;
+    }
+}
